3b3fe17c0776041e8a15ca7804195733402080d5
AugustusB <A.Balasuriya@btinternet.com>
2018-11-20 08:14:23 +0000

    the this key work study

---
+++ scripts/jsCall.js
@@ -0,0 +1,75 @@
+
+(function(ns1, ns2){
+    "use strict"
+    ns2.myObjVar1 = { myVar1: "v1", msgO: "this is bound to myObjVar1: "};
+    ns2.myObjVar2 = { myVar1: "v2", msgO: "this is bound to myObjVar2: "};
+    ns2.myVar1 = "v3";
+
+    ns2.myObjLiteral1 = {
+        animaltype: "Dog",
+        age: 2,
+        food: "meat",
+        speak: function speak(){
+            return `I am a ${this.animaltype}, I am ${this.age} years old and I eat ${this.food}`;
+        }
+    }     
+
+    ns2.ExplicitBinding = function ExplicitBinding(){
+        //Private members
+        var testCall1 = function testCall1(){
+            return `${this.msgO} ${this.myVar1}`;
+        },
+        callDisplay1 = function callDisplay1(findElem, str){
+            $(findElem).append(`<li>jsExplicitBinding.testCall1.call(Gus.JS2.myObjVar1) = ${str} </li>`);
+
+        },
+        callDisplay2 = function callDisplay2(findElem, str){
+            $(findElem).append(`<li>jsExplicitBinding.testCall1.call(Gus.JS2.myObjVar2) = ${str} </li>`);
+
+        },
+        testApply1 = function testApply1(msg){
+            return `${msg} ${this.msgO} ${this.myVar1}`;
+        },
+        testApply1Display1 = function testApply1Display1(findElem, str){
+            $(findElem).append(`<li>${str} </li>`);
+
+        },
+        testBind1 = function testBind1(msg){
+            return `${msg}  ${this.msgO} ${this.myVar1}`;
+        },
+        testBind1Display1 = function testBind1Display1(findElem, str){
+            $(findElem).append(`<li>${str} </li>`);
+
+        },
+
+        testmyObjLiteral1Display1 = function testmyObjLiteral1Display1(findElem, str){
+            $(findElem).append(`<li>myObjLiteral1.speak = ${str} </li>`);
+        },
+
+        testmyObjLiteral1Display2 = function testmyObjLiteral1Display2(findElem, obj) {
+            $(findElem).append(`<li>myObjLiteral2.speak call in another function = ${obj.speak()} </li>`);
+        };
+        
+        //Public members 
+        return {
+            testCall1: testCall1,
+            callDisplay1: callDisplay1,
+            callDisplay2: callDisplay2,
+            testApply1: testApply1,
+            testApply1Display1: testApply1Display1,
+            testBind1:testBind1,
+            testBind1Display1:testBind1Display1,
+            testmyObjLiteral1Display1: testmyObjLiteral1Display1,
+            testmyObjLiteral1Display2:testmyObjLiteral1Display2
+
+            // Numbers2 : Numbers2,
+            // Numbers3 : Numbers3,
+            // Numbers4 : Numbers4,
+            // Numbers5 : Numbers5,
+            // Numbers6 : Numbers6
+        };
+
+    };
+
+})(window.Gus = window.Gus || {}, 
+    window.Gus.JS2 = window.Gus.JS2 || {});
---
+++ scripts/jsCallMain.js
@@ -0,0 +1,78 @@
+"use strict"
+$(function(){
+    try{
+        var jsExplicitBinding = new Gus.JS2.ExplicitBinding();
+
+        var str = jsExplicitBinding.testCall1.call(Gus.JS2.myObjVar1);
+        jsExplicitBinding.callDisplay1('#call1 ol', str);
+
+        str = jsExplicitBinding.testCall1.call(Gus.JS2.myObjVar2);
+        jsExplicitBinding.callDisplay2('#call1 ol', str);
+
+        str = jsExplicitBinding.testApply1.apply(Gus.JS2.myObjVar1, ["jsExplicitBinding.testApply1.apply(Gus.JS2.myObjVar1) = "]);
+        jsExplicitBinding.testApply1Display1('#apply1 ol', str);
+
+        str = jsExplicitBinding.testApply1.apply(Gus.JS2.myObjVar2, ["jsExplicitBinding.testApply1.apply(Gus.JS2.myObjVar2) = "]);
+        jsExplicitBinding.testApply1Display1('#apply1 ol', str);
+
+        str = jsExplicitBinding.testBind1.bind(Gus.JS2.myObjVar1, ["jsExplicitBinding.testBind1.bind(Gus.JS2.myObjVar1) = "])();
+        jsExplicitBinding.testBind1Display1('#bind1 ol', str);
+
+        str = jsExplicitBinding.testBind1.bind(Gus.JS2.myObjVar2, ["jsExplicitBinding.testBind1.bind(Gus.JS2.myObjVar2) = "])();
+        jsExplicitBinding.testBind1Display1('#bind1 ol', str);
+
+        str = Gus.JS2.myObjLiteral1.speak();
+        jsExplicitBinding.testmyObjLiteral1Display1('#objLiteral ol', str);
+
+        jsExplicitBinding.testmyObjLiteral1Display2('#objLiteral ol', Gus.JS2.myObjLiteral1);
+        
+
+    }catch(error)
+    {
+        $('#objLiteral ol').append("<li>Caught error : " + error + "</li>");
+    }
+    // var jsExplicitBinding = new Gus.JS2.ExplicitBinding();
+    // var str = jsExplicitBinding.Call1.call(Gus.JS2.myObjVar1);
+    // jsExplicitBinding.CallDisplay1('#call1 ol', str);
+    // str = jsExplicitBinding.Call1.call(Gus.JS2.myObjVar2);
+    // jsExplicitBinding.CallDisplay2('#call1 ol', str);
+    // str = jsExplicitBinding.foo();
+    // jsExplicitBinding.fooDisplay1('#call1 ol', str);
+    // jsExplicitBinding.Call1('#call1 ol');
+    // jsExplicitBinding.Numbers2('#number ol');
+    // jsExplicitBinding.Numbers3('#number ol');
+    // jsExplicitBinding.Numbers4('#number ol');
+    // jsExplicitBinding.Numbers5('#number ol');
+    // jsExplicitBinding.Numbers6('#number ol');
+
+    // var jsString = new Gus.JS2.String();
+    // jsString.String1('#string ol');
+
+    // var jsBoolean = new Gus.JS2.Boolean();
+    // jsBoolean.boolean1('#boolean ol');
+
+    // var es6NewSyntax = new Gus.JS3.NewSytax();
+    // es6NewSyntax.newLet1('#let ol');
+    // es6NewSyntax.newLet2('#let ol');
+    // es6NewSyntax.newLet3('#let ol');
+    // es6NewSyntax.newLet4('#let ol');
+    // es6NewSyntax.newLet5('#let ol');
+    // es6NewSyntax.newLet6('#let ol');
+    // es6NewSyntax.newConst7('#constant ol');
+    // es6NewSyntax.jsLambda1('#fatArrow ol');
+    // es6NewSyntax.jsLambda2('#fatArrow ol');
+    // es6NewSyntax.jsLambda3('#fatArrow ol');
+    // es6NewSyntax.jsLambda4('#fatArrow ol');
+    // es6NewSyntax.jsFuncTestThis('#fatArrow ol');
+    // es6NewSyntax.jsFuncTestThis1('#fatArrow ol', true);
+    // es6NewSyntax.jsFuncTestThis2('#fatArrow ol');
+    // es6NewSyntax.jsDynamicFunction('#dynamicFunction ol');
+    // es6NewSyntax.jsDefaultParaFunc1('#defaultPara ol');
+    // es6NewSyntax.jsRestOperator('#rest ol', 123, 'veg', 'meat', 'pasta');
+    // es6NewSyntax.jsSpreadOperator('#spread ol');
+    // es6NewSyntax.jsObjectLiteralsEx('#object ol');;
+    // es6NewSyntax.jsForOfLoop('#forOfLoop ol');
+    // es6NewSyntax.jsOctalBinary('#octalAndBinary ol');
+    // es6NewSyntax.jsTemplateLiterals('#temporalLiteral ol');
+    // es6NewSyntax.jsDestructuring('#destructuring ol');
+});
